89e9707819850072dccb70cab8c18114e335e2c2
Rada <pfb2024@info22.cshl.edu>
2024-10-15 15:49:05 -0400

    else if practice

---
+++ python1/elifscript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+count = 15
+if count < 10:
+  message = "Not True it is less than 10"
+  print (count, message)
+elif count > 10:
+  message = "AYE Captain it is more than 10"
+  print (count, message)
+  
