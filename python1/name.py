ac07f692c77dbbf7b2689719ab8eb73fbdb36a13
Rada <pfb2024@info22.cshl.edu>
2024-10-15 12:07:48 -0400

    Python 1 problem set

--- python1/name.py
+++ python1/name.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
-print("rada")
+import sys
+
+name = 'rada'
+print(name)
+favcolor = 'blue'
+print(favcolor)
+activity = 'painting'
+print(activity)
+favanimal = 'dogs'
+print(favanimal)
+
+print(f"My name: {name}")
+
+name = sys.argv[1]
+favcolor = sys.argv[2]
+activity = sys.argv[3]
+favanimal = sys.argv[4]
+print('my name is', name, 'my favorite color is', favcolor, 'my favorite activity is', activity, 'my favorite animals are', favanimal)
+
