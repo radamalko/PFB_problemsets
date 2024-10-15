ea7e76585af14d9c18f69c9f02a25cd587a9eb34
Rada <pfb2024@info22.cshl.edu>
2024-10-15 16:07:06 -0400

    q3posneg

---
+++ python1/posneg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+count = -1
+if count > 0:
+  message = "positive"
+  print(count, message)
+elif count < 0:
+  message = "negative"
+  print(count, message)
+else:
+  print('must be 0')
+
